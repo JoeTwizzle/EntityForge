795a1fb3798de70604bc9f483b3c14bf1ea4b5f2
JoeTwizzle <the3.14159268483279@gmail.com>
2022-12-04 02:21:23 +0100

    Update IComponent.cs

--- Archie/Core/IComponent.cs
+++ Archie/Core/IComponent.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace Archie
 {
-    public interface IComponent<T> where T : struct, IComponent<T>
+    //Constrain to where T : this if the language allows it
+    public interface IComponent<T> where T : struct, IComponent<T> 
     {
         static virtual void Init(ref T self) { }
         static virtual void Del(ref T self) { }
