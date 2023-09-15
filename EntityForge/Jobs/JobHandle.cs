425643cbe22ca82e3e3330371b4dd93b39455798
JoeTwizzle <the3.14159268483279@gmail.com>
2023-09-15 18:42:08 +0200

    Remove Jobs until more mature and working fully

--- EntityForge/Jobs/JobHandle.cs
+++ EntityForge/Jobs/JobHandle.cs
@@ -1,85 +1,85 @@
-﻿namespace EntityForge.Jobs
-{
-    public readonly struct JobHandle : IEquatable<JobHandle>
-    {
-        public bool IsCompleted
-        {
-            get
-            {
-                //check if our scheduler has been disposed and is oob
+﻿//namespace EntityForge.Jobs
+//{
+//    public readonly struct JobHandle : IEquatable<JobHandle>
+//    {
+//        public bool IsCompleted
+//        {
+//            get
+//            {
+//                //check if our scheduler has been disposed and is oob
 
-                JobThreadScheduler.SchedulersArrayLock.EnterReadLock();
-                var scheduler = JobThreadScheduler.Schedulers[schedulerId];
-                JobThreadScheduler.SchedulersArrayLock.ExitReadLock();
-                if (scheduler == null)
-                {
-                    return true;
-                }
-                if (scheduler.Version == version)
-                {
-                    if (scheduler.maxCompletedJobId >= id)
-                    {
-                        return true;
-                    }
-                    else if (scheduler.jobCounter > id)
-                    {
-                        return false;
-                    }
-                    scheduler.rwLock.EnterReadLock();
-                    var status = scheduler.activeJobs[(id - scheduler.maxCompletedJobId) - 1];
-                    scheduler.rwLock.ExitReadLock();
-                    return status;
-                }
-                return scheduler.Version > version;
-            }
-        }
+//                JobThreadScheduler.SchedulersArrayLock.EnterReadLock();
+//                var scheduler = JobThreadScheduler.Schedulers[schedulerId];
+//                JobThreadScheduler.SchedulersArrayLock.ExitReadLock();
+//                if (scheduler == null)
+//                {
+//                    return true;
+//                }
+//                if (scheduler.Version == version)
+//                {
+//                    if (scheduler.maxCompletedJobId >= id)
+//                    {
+//                        return true;
+//                    }
+//                    else if (scheduler.jobCounter > id)
+//                    {
+//                        return false;
+//                    }
+//                    scheduler.rwLock.EnterReadLock();
+//                    var status = scheduler.activeJobs[(id - scheduler.maxCompletedJobId) - 1];
+//                    scheduler.rwLock.ExitReadLock();
+//                    return status;
+//                }
+//                return scheduler.Version > version;
+//            }
+//        }
 
-        private readonly short schedulerId;
-        private readonly short version;
-        private readonly int id;
+//        private readonly short schedulerId;
+//        private readonly short version;
+//        private readonly int id;
 
-        public JobHandle(short schedulerId, short version, int id)
-        {
-            this.schedulerId = schedulerId;
-            this.version = version;
-            this.id = id;
-        }
+//        public JobHandle(short schedulerId, short version, int id)
+//        {
+//            this.schedulerId = schedulerId;
+//            this.version = version;
+//            this.id = id;
+//        }
 
-        public void WaitForCompletion()
-        {
-            var sw = new SpinWait();
-            while (!IsCompleted)
-            {
-                sw.SpinOnce();
-            }
-        }
+//        public void WaitForCompletion()
+//        {
+//            var sw = new SpinWait();
+//            while (!IsCompleted)
+//            {
+//                sw.SpinOnce();
+//            }
+//        }
 
-        public override bool Equals(object? obj)
-        {
-            return obj is JobHandle h && Equals(h);
-        }
+//        public override bool Equals(object? obj)
+//        {
+//            return obj is JobHandle h && Equals(h);
+//        }
 
-        public override int GetHashCode()
-        {
-            int hash = 17;
-            hash = hash * 486187739 + schedulerId;
-            hash = hash * 486187739 + id;
-            return hash;
-        }
+//        public override int GetHashCode()
+//        {
+//            int hash = 17;
+//            hash = hash * 486187739 + schedulerId;
+//            hash = hash * 486187739 + id;
+//            return hash;
+//        }
 
-        public static bool operator ==(JobHandle left, JobHandle right)
-        {
-            return left.Equals(right);
-        }
+//        public static bool operator ==(JobHandle left, JobHandle right)
+//        {
+//            return left.Equals(right);
+//        }
 
-        public static bool operator !=(JobHandle left, JobHandle right)
-        {
-            return !(left == right);
-        }
+//        public static bool operator !=(JobHandle left, JobHandle right)
+//        {
+//            return !(left == right);
+//        }
 
-        public bool Equals(JobHandle other)
-        {
-            return id == other.id && schedulerId == other.schedulerId;
-        }
-    }
-}
+//        public bool Equals(JobHandle other)
+//        {
+//            return id == other.id && schedulerId == other.schedulerId;
+//        }
+//    }
+//}
--- EntityForge/Jobs/JobThreadScheduler.cs
+++ EntityForge/Jobs/JobThreadScheduler.cs
@@ -1,183 +1,183 @@
-﻿using EntityForge.Helpers;
-using System.Collections.Concurrent;
-using System.Numerics;
+﻿//using EntityForge.Helpers;
+//using System.Collections.Concurrent;
+//using System.Numerics;
 
-namespace EntityForge.Jobs
-{
-    public sealed class JobThreadScheduler : IDisposable
-    {
-        internal static readonly ReaderWriterLockSlim SchedulersArrayLock = new();
+//namespace EntityForge.Jobs
+//{
+//    public sealed class JobThreadScheduler : IDisposable
+//    {
+//        internal static readonly ReaderWriterLockSlim SchedulersArrayLock = new();
 
-        internal static JobThreadScheduler[] Schedulers { get; private set; } = new JobThreadScheduler[1];
+//        internal static JobThreadScheduler[] Schedulers { get; private set; } = new JobThreadScheduler[1];
 
-        private static short schedulerCounter;
+//        private static short schedulerCounter;
 
-        private static readonly Queue<short> recycledIds = new();
+//        private static readonly Queue<short> recycledIds = new();
 
-        private readonly short id;
-        internal short Version;
-
-
-        private readonly Thread[] workerThreads;
-
-        private readonly CancellationTokenSource cancellationTokenSource;
-
-        private readonly ConcurrentQueue<(Action job, int jobId)> jobs;
-
-        internal readonly ReaderWriterLockSlim rwLock = new();
-
-        internal int maxCompletedJobId;
-
-        internal int jobCounter;
+//        private readonly short id;
+//        internal short Version;
 
 
-        private SpinLock spinLock;
-        //Sliding window of active/completed jobs
-        //index = id - maxCompletedJobId
-        internal bool[] activeJobs = Array.Empty<bool>();
+//        private readonly Thread[] workerThreads;
 
-        public JobThreadScheduler()
-        {
-            jobs = new();
-            cancellationTokenSource = new();
-            workerThreads = new Thread[Math.Max(1, Environment.ProcessorCount - 1)];
-            for (int i = 0; i < workerThreads.Length; i++)
-            {
-                workerThreads[i] = new Thread(() => WorkerLoop(cancellationTokenSource.Token)) { IsBackground = true, Name = $"Worker {i + 1}" };
-            }
-            //register in static array
-            SchedulersArrayLock.EnterWriteLock();
-            if (!recycledIds.TryDequeue(out short id))
-            {
-                id = schedulerCounter++;
-            }
-            this.id = id;
+//        private readonly CancellationTokenSource cancellationTokenSource;
 
-            Schedulers = Schedulers.EnsureContains(id);
-            Schedulers[id] = this;
-            SchedulersArrayLock.ExitWriteLock();
-            //start all worker threads
-            for (int i = 0; i < workerThreads.Length; i++)
-            {
-                workerThreads[i].Start();
-            }
-        }
+//        private readonly ConcurrentQueue<(Action job, int jobId)> jobs;
 
-        void WorkerLoop(CancellationToken token)
-        {
-            var sw = new SpinWait();
-            while (!token.IsCancellationRequested)
-            {
-                //Get job if available
-                if (jobs.TryDequeue(out var action))
-                {
-                    //execute job
-                    action.job();
-                    //read jobCounter
-                    rwLock.EnterUpgradeableReadLock();
-                    int maxJob = maxCompletedJobId;
-                    int requiredSlidingWindowSize = jobCounter - maxJob;
-                    int activeJobsIndex = (action.jobId - maxJob) - 1;
+//        internal readonly ReaderWriterLockSlim rwLock = new();
 
-                    //check if we are the first item in this array and the array has items
-                    if (activeJobsIndex == 0)
-                    {
-                        rwLock.EnterWriteLock();
-                        //Scan for last job that was completed from left to right of array
-                        int afterLastCompletedJobIndex = 1;
-                        for (; afterLastCompletedJobIndex < activeJobs.Length && activeJobs[afterLastCompletedJobIndex]; afterLastCompletedJobIndex++) ;
-                        //move all uncompleted items to the left
-                        if (activeJobs.Length > 0)
-                        {
-                            Array.Copy(activeJobs, afterLastCompletedJobIndex, activeJobs, 0, activeJobs.Length - afterLastCompletedJobIndex);
-                        }
-                        rwLock.ExitWriteLock();
-                        Interlocked.Add(ref maxCompletedJobId, afterLastCompletedJobIndex);
-                    }
-                    else //we are somewhere in the middle of the array
-                    {
-                        //check if we need to grow to fit in the array
-                        rwLock.EnterWriteLock();
-                        if (activeJobs.Length <= requiredSlidingWindowSize)
-                        {
-                            Array.Resize(ref activeJobs, (int)BitOperations.RoundUpToPowerOf2((uint)requiredSlidingWindowSize + 1));
-                        }
-                        // mark job as completed
-                        activeJobs[activeJobsIndex] = true;
-                        rwLock.ExitWriteLock();
-                    }
-                    rwLock.ExitUpgradeableReadLock();
-                }
-                else //no job was available
-                {
-                    //wait a bit
-                    sw.SpinOnce();
-                }
-                // check if we will switch context next spin
-                if (sw.NextSpinWillYield)
-                {
-                    //Spin and switch contexts
-                    sw.SpinOnce();
-                    //reset so we do not switch contexts too often.
-                    //this does waste a few cpu cycles for lower latency.
-                    sw.Reset();
-                }
-            }
-        }
+//        internal int maxCompletedJobId;
+
+//        internal int jobCounter;
 
 
-        public JobHandle Schedule(Action job)
-        {
-            var jobId = Interlocked.Increment(ref jobCounter);
-            //To deal with int overflow reset counter to 0
-            bool idReset = false;
-            spinLock.Enter(ref idReset);
-            if (jobId == int.MaxValue - 20000)
-            {
-                WaitForActiveJobsCompletion();
-                jobCounter = 1;
-                jobId = jobCounter;
-                maxCompletedJobId = 0;
-                Version++;
-            }
-            if (idReset)
-            {
-                spinLock.Exit();
-            }
-            var handle = new JobHandle(id, Version, jobId);
-            jobs.Enqueue((job, jobId));
-            return handle;
-        }
+//        private SpinLock spinLock;
+//        //Sliding window of active/completed jobs
+//        //index = id - maxCompletedJobId
+//        internal bool[] activeJobs = Array.Empty<bool>();
 
-        void WaitForActiveJobsCompletion()
-        {
-            var sw = new SpinWait();
-            while (maxCompletedJobId != jobCounter)
-            {
-                sw.SpinOnce();
-            }
-        }
+//        public JobThreadScheduler()
+//        {
+//            jobs = new();
+//            cancellationTokenSource = new();
+//            workerThreads = new Thread[Math.Max(1, Environment.ProcessorCount - 1)];
+//            for (int i = 0; i < workerThreads.Length; i++)
+//            {
+//                workerThreads[i] = new Thread(() => WorkerLoop(cancellationTokenSource.Token)) { IsBackground = true, Name = $"Worker {i + 1}" };
+//            }
+//            //register in static array
+//            SchedulersArrayLock.EnterWriteLock();
+//            if (!recycledIds.TryDequeue(out short id))
+//            {
+//                id = schedulerCounter++;
+//            }
+//            this.id = id;
 
-        public void Dispose()
-        {
-            //Signal threads to exit after current iteration
-            cancellationTokenSource.Cancel();
-            WaitForActiveJobsCompletion();
-            //discard all pending jobs
-            jobs.Clear();
-            //make sure all threads have finished their work
-            for (int i = 0; i < workerThreads.Length; i++)
-            {
-                workerThreads[i].Join();
-            }
-            //unregister from static array
-            SchedulersArrayLock.EnterWriteLock();
-            Schedulers[id] = null!;
-            recycledIds.Enqueue(id);
-            SchedulersArrayLock.ExitWriteLock();
-            //dispose resources
-            cancellationTokenSource.Dispose();
-            rwLock.Dispose();
-        }
-    }
-}
+//            Schedulers = Schedulers.EnsureContains(id);
+//            Schedulers[id] = this;
+//            SchedulersArrayLock.ExitWriteLock();
+//            //start all worker threads
+//            for (int i = 0; i < workerThreads.Length; i++)
+//            {
+//                workerThreads[i].Start();
+//            }
+//        }
+
+//        void WorkerLoop(CancellationToken token)
+//        {
+//            var sw = new SpinWait();
+//            while (!token.IsCancellationRequested)
+//            {
+//                //Get job if available
+//                if (jobs.TryDequeue(out var action))
+//                {
+//                    //execute job
+//                    action.job();
+//                    //read jobCounter
+//                    rwLock.EnterUpgradeableReadLock();
+//                    int maxJob = maxCompletedJobId;
+//                    int requiredSlidingWindowSize = jobCounter - maxJob;
+//                    int activeJobsIndex = (action.jobId - maxJob) - 1;
+
+//                    //check if we are the first item in this array and the array has items
+//                    if (activeJobsIndex == 0)
+//                    {
+//                        rwLock.EnterWriteLock();
+//                        //Scan for last job that was completed from left to right of array
+//                        int afterLastCompletedJobIndex = 1;
+//                        for (; afterLastCompletedJobIndex < activeJobs.Length && activeJobs[afterLastCompletedJobIndex]; afterLastCompletedJobIndex++) ;
+//                        //move all uncompleted items to the left
+//                        if (activeJobs.Length > 0)
+//                        {
+//                            Array.Copy(activeJobs, afterLastCompletedJobIndex, activeJobs, 0, activeJobs.Length - afterLastCompletedJobIndex);
+//                        }
+//                        rwLock.ExitWriteLock();
+//                        Interlocked.Add(ref maxCompletedJobId, afterLastCompletedJobIndex);
+//                    }
+//                    else //we are somewhere in the middle of the array
+//                    {
+//                        //check if we need to grow to fit in the array
+//                        rwLock.EnterWriteLock();
+//                        if (activeJobs.Length <= requiredSlidingWindowSize)
+//                        {
+//                            Array.Resize(ref activeJobs, (int)BitOperations.RoundUpToPowerOf2((uint)requiredSlidingWindowSize + 1));
+//                        }
+//                        // mark job as completed
+//                        activeJobs[activeJobsIndex] = true;
+//                        rwLock.ExitWriteLock();
+//                    }
+//                    rwLock.ExitUpgradeableReadLock();
+//                }
+//                else //no job was available
+//                {
+//                    //wait a bit
+//                    sw.SpinOnce();
+//                }
+//                // check if we will switch context next spin
+//                if (sw.NextSpinWillYield)
+//                {
+//                    //Spin and switch contexts
+//                    sw.SpinOnce();
+//                    //reset so we do not switch contexts too often.
+//                    //this does waste a few cpu cycles for lower latency.
+//                    sw.Reset();
+//                }
+//            }
+//        }
+
+
+//        public JobHandle Schedule(Action job)
+//        {
+//            var jobId = Interlocked.Increment(ref jobCounter);
+//            //To deal with int overflow reset counter to 0
+//            bool idReset = false;
+//            spinLock.Enter(ref idReset);
+//            if (jobId == int.MaxValue - 20000)
+//            {
+//                WaitForActiveJobsCompletion();
+//                jobCounter = 1;
+//                jobId = jobCounter;
+//                maxCompletedJobId = 0;
+//                Version++;
+//            }
+//            if (idReset)
+//            {
+//                spinLock.Exit();
+//            }
+//            var handle = new JobHandle(id, Version, jobId);
+//            jobs.Enqueue((job, jobId));
+//            return handle;
+//        }
+
+//        void WaitForActiveJobsCompletion()
+//        {
+//            var sw = new SpinWait();
+//            while (maxCompletedJobId != jobCounter)
+//            {
+//                sw.SpinOnce();
+//            }
+//        }
+
+//        public void Dispose()
+//        {
+//            //Signal threads to exit after current iteration
+//            cancellationTokenSource.Cancel();
+//            WaitForActiveJobsCompletion();
+//            //discard all pending jobs
+//            jobs.Clear();
+//            //make sure all threads have finished their work
+//            for (int i = 0; i < workerThreads.Length; i++)
+//            {
+//                workerThreads[i].Join();
+//            }
+//            //unregister from static array
+//            SchedulersArrayLock.EnterWriteLock();
+//            Schedulers[id] = null!;
+//            recycledIds.Enqueue(id);
+//            SchedulersArrayLock.ExitWriteLock();
+//            //dispose resources
+//            cancellationTokenSource.Dispose();
+//            rwLock.Dispose();
+//        }
+//    }
+//}
